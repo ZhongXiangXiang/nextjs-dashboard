44071260344241e676b42cc83ee0b78fbcee404d
ZhongXiangXiang <2366154235@qq.com>
2025-02-09 22:33:58 +0800

    Handling errors: add error and not-found UI

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -30,10 +30,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.log('create invoice error', error);
+  }
 
   revalidatePath('/dashboard/invoices'); // 清除浏览器路由缓存，重新向服务器发起请求获取最新数据
   redirect('/dashboard/invoices');
@@ -49,17 +53,23 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id=${customerId}, amount=${amountInCents}, status=${status}
-    WHERE id=${id}
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id=${customerId}, amount=${amountInCents}, status=${status}
+      WHERE id=${id}
+    `;
+  } catch (error) {
+    console.log('update invoice error', error);
+  }
 
   revalidatePath('/dashboard/invoices'); // to clear the client cache and make a new server request
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice');
+
   await sql`DELETE FROM invoices WHERE id=${id}`;
   revalidatePath('/dashboard/invoices');
 }
